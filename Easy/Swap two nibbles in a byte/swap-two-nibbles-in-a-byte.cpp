8451a917efe818588cb74a1c3ace43f04138e473
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-05-31 18:05:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Swap two nibbles in a byte/swap-two-nibbles-in-a-byte.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        int a = n % 16;  
+        int b = n / 16; 
+        return a*16 + b;  
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.swapNibbles(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
