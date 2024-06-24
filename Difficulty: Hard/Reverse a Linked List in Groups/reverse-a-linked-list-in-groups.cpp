029d049dd00a41516a0c3859dd0bc4d40b988aae
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-06-24 11:58:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Reverse a Linked List in Groups/reverse-a-linked-list-in-groups.cpp
@@ -0,0 +1,135 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    int lenOfLL(node* head) {
+        node* temp = head;
+        int count = 0;
+        while(temp != NULL) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+    node* reverseInKGroups(node* head, int k) {
+        node* prev = NULL;
+        node* curr = head;
+        node* nextNode = nullptr;
+        int count = 0;
+        // if(lenOfLL(head)<k) {
+        //     return head;
+        // }
+        if(curr == NULL) return head;
+        
+        while(count<k && curr != NULL) {
+            nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+            count++;
+        }
+        if(nextNode != NULL) {
+            head->next = reverseInKGroups(nextNode, k);
+        }
+        return prev;
+    }
+    node *reverseIt (node *head, int k)
+    { 
+        // Complete this method
+        node* ansHead = reverseInKGroups(head, k);
+        return ansHead;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverseIt(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+
+// } Driver Code Ends
