cadc2269a78954a4a6009c307bec90a1bd219af9
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-09-01 22:32:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -1,89 +1,62 @@
 //{ Driver Code Starts
-//Initial function template for C++
+// Initial function template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-//User function template for C++
-// arr : given array
-    // l : starting index of the array i.e 0
-    // r : ending index of the array i.e size-1
-    // k : find kth smallest element and return using this function
+// User function template for C++
 
-
-// Approach 1 : Using sort function :
-// class Solution{
-//     public:
-//     int kthSmallest(int arr[], int l, int r, int k) {
-//         sort(arr, arr + r+1);
-//         return arr[k-1];
-//     }
-// };
-
-// Approach 2 : Using priority queue : S.C : O(n)
-// class Solution{
-//     public:
-//     int kthSmallest(int arr[], int l, int r, int k) {
-//         //code here
-//         priority_queue<int, vector<int>, greater<int>> pq;
-//         for(int i=l; i<=r; i++) {
-//             pq.push(arr[i]);
-//         }
-//         while(k-1) {
-//             pq.pop();
-//             k--;
-//         }
-//         return pq.top();
-//     }
-// };
-
-// Approach 3 : More efficient : S.C. : O(k)
 class Solution {
-public:
-    int kthSmallest(int arr[], int l, int r, int k) {
-        // Max heap
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        int n = arr.size();
         priority_queue<int> pq;
-
-        // Store the first k elements in the priority queue
-        for(int i = l; i < l + k; i++) {
+        
+        for(int i=0; i<n; i++) {
             pq.push(arr[i]);
         }
-
-        // Process the remaining elements
-        for(int i = l + k; i <= r; i++) {
-            if(arr[i] < pq.top()) {
-                pq.pop();
-                pq.push(arr[i]);
-            }
+        
+        for(int i=k; i<n; i++) {
+            pq.pop();
         }
 
         return pq.top();
     }
 };
 
-
-
 //{ Driver Code Starts.
- 
-int main()
-{
+
+int main() {
     int test_case;
-    cin>>test_case;
-    while(test_case--)
-    {
-        int number_of_elements;
-        cin>>number_of_elements;
-        int a[number_of_elements];
-        
-        for(int i=0;i<number_of_elements;i++)
-            cin>>a[i];
-            
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
         int k;
-        cin>>k;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        k = crr[0];
+        int n = arr.size();
         Solution ob;
-        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+        cout << ob.kthSmallest(arr, k) << endl;
     }
     return 0;
 }
+
 // } Driver Code Ends
