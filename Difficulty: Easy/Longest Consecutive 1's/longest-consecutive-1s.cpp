9ed3741188cc9145ffed56c3473b1751b5f1c0fc
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-02-20 16:29:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Longest Consecutive 1's/longest-consecutive-1s.cpp
+++ Difficulty: Easy/Longest Consecutive 1's/longest-consecutive-1s.cpp
@@ -11,33 +11,24 @@ using namespace std;
 /*  Function to calculate the longest consecutive ones
 *   N: given input to calculate the longest consecutive ones
 */
-
 class Solution
 {
-    private:
-    string decToBin(int n) {
-        string str;
-        while(n>0) {
-            str += to_string(n%2);
-            n /= 2;
-        }
-        return str;
-    }
-
     public:
-    int maxConsecutiveOnes(int n)
+    // most optimized approach : Logarithmic time complexity : 
+    int maxConsecutiveOnes(int N)
     {
-        // code here
-        string str = decToBin(n);
-        int count = 0, maxConsOne = 0;
-        
-        for(int i=0; i <str.size(); i++) {
-            if(str[i] == '1') count++;
+        int num = N;
+        int count = 0;
+        int ans = 0;
+        while(num > 0) {
+            if(num & 1) {
+                count++;
+                ans = max(ans, count);
+            }
             else count = 0;
-            maxConsOne = max(maxConsOne, count);
+            num >>= 1;                  // right shift operator
         }
-        
-        return maxConsOne;
+        return ans;
     }
 };
 
@@ -55,7 +46,9 @@ int main() {
 		Solution obj;
 		//calling maxConsecutiveOnes() function
 		cout<<obj.maxConsecutiveOnes(n)<<endl;
-	}
+	
+cout << "~" << "\n";
+}
 	return 0;
 }
 // } Driver Code Ends
