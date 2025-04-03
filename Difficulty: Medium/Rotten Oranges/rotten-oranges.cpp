07ebe9f61b0c2ef8c58a39df3ab8dbec5540719b
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-04-03 18:34:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
@@ -1,8 +1,10 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 
 class Solution 
 {
@@ -64,22 +66,25 @@ class Solution
 
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<int>>grid(n, vector<int>(m, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-				cin >> grid[i][j];
-			}
-		}
-		Solution obj;
-		int ans = obj.orangesRotting(grid);
-		cout << ans << "\n";
-	}
-	return 0;
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> mat(n, vector<int>(m, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> mat[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.orangesRotting(mat);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
