3d95e01a25277ac8926616d2e2a1e7f981a8b6e6
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-07-10 10:55:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.cpp
@@ -0,0 +1,38 @@
+// User function Template for C++
+
+/*
+class Node{
+public:
+    Node* prev;
+    int data;
+    Node* next;
+
+    Node()
+    {
+        prev = NULL;
+        data = 0;
+        next = NULL;
+    }
+
+    Node(int value)
+    {
+        prev = NULL;
+        data = value;
+        next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructDLL(vector<int>& arr) {
+        Node* head = new Node(arr[0]);
+        Node* mover = head;
+        for(int i=1; i<arr.size(); i++) {
+            Node* temp = new Node(arr[i]);
+            mover->next = temp;
+            temp->prev = mover;
+            mover = temp;
+        }
+        return head;
+    }
+};
