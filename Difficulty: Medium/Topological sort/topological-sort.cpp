38a9a5ff7e28f7d226915e55afe81238847df6ab
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-07-19 09:49:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,102 +1,38 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 
 class Solution {
+  private:
+    void dfs(int node, int V, stack<int>& st, vector<int>& vis,
+             vector<vector<int>>& adjList) {
+        vis[node] = 1;  // Mark current node as visited
+        // Traverse all unvisited neighbors
+        for(auto nbr : adjList[node]) {
+            if(!vis[nbr])
+                dfs(nbr, V, st, vis, adjList);
+        }
+        // After visiting all neighbors, push the node to stack
+        st.push(node);
+    }
   public:
-    // Function to return list containing vertices in Topological order.
-    vector<int> topologicalSort(vector<vector<int>>& adj) {
-        // Your code here
-        int n = adj.size();
-        
-        queue<int> q;
-        vector<int> indegree(n, 0);
-        vector<int> topoOrder;
-        
-        // storing the indegrees : 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<adj[i].size(); j++) {
-                indegree[adj[i][j]]++;
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // Step 1: Build adjacency list from the edge list
+        vector<vector<int>> adjList(V);
+        for(int i = 0; i < edges.size(); i++) {
+            adjList[edges[i][0]].push_back(edges[i][1]);
+        }
+        stack<int> st;         // Stack to store the topological order
+        vector<int> vis(V, 0); // Visited array
+        // Step 2: Call DFS for all unvisited nodes
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                dfs(i, V, st, vis, adjList);
             }
         }
-        
-        // pushing the zero indegree nodes in the queue : 
-        for(int i=0; i<n; i++) {
-            if(indegree[i] == 0) {
-                q.push(i);
-            }
+        // Step 3: Pop elements from stack to get topological order
+        vector<int> ans;
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
         }
-        
-        // actual bfs logic : 
-        while(!q.empty()) {
-            int frontNode = q.front();
-            q.pop();
-            topoOrder.push_back(frontNode);
-            
-            for(int i=0; i<adj[frontNode].size(); i++) {
-                indegree[adj[frontNode][i]]--;
-                if(indegree[adj[frontNode][i]] == 0) {
-                    q.push(adj[frontNode][i]);
-                }
-            }
-        }
-        
-        return topoOrder;
+        return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-/*  Function to check if elements returned by user
- *   contains the elements in topological sorted form
- *   V: number of vertices
- *   *res: array containing elements in topological sorted form
- *   adj[]: graph input
- */
-int check(int V, vector<int> &res, vector<vector<int>> adj) {
-
-    if (V != res.size())
-        return 0;
-
-    vector<int> map(V, -1);
-    for (int i = 0; i < V; i++) {
-        map[res[i]] = i;
-    }
-    for (int i = 0; i < V; i++) {
-        for (int v : adj[i]) {
-            if (map[i] > map[v])
-                return 0;
-        }
-    }
-    return 1;
-}
-
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int N, E;
-        cin >> E >> N;
-        int u, v;
-
-        vector<vector<int>> adj(N);
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-        }
-
-        Solution obj;
-        vector<int> res = obj.topologicalSort(adj);
-
-        cout << check(N, res, adj) << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
