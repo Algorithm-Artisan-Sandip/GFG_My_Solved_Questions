36fcab57dffb64d9e06d156182814994767cb3fd
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-07-20 21:17:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Weighted undirected graph/shortest-path-in-weighted-undirected-graph.cpp
+++ Difficulty: Medium/Shortest Path in Weighted undirected graph/shortest-path-in-weighted-undirected-graph.cpp
@@ -1,95 +1,57 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 
 class Solution {
   public:
     vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
-        // Code here
-        vector<pair<int,int>>adj[n+1];
-        for(auto it:edges){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
+        // Step 1: Create an adjacency list where each node stores a pair of (neighbour, weight)
+        vector<vector<pair<int, int>>> adjList(n + 1);
+        for (auto it : edges) {
+            adjList[it[0]].push_back({it[1], it[2]}); // Undirected edge
+            adjList[it[1]].push_back({it[0], it[2]});
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,
-            greater<pair<int,int>>>pq;
-        
-        pq.push({0,1});
-        vector<int>dis(n+1,INT_MAX),par(n+1);
-        for(int i=1; i<n+1;i++){
-            par[i]=i;
+        // Step 2: Min-heap (priority queue) to pick the next node with the shortest distance
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        // Push the source node (1) with distance 0
+        pq.push({0, 1});
+        // Step 3: Distance array to store shortest distance from source to every node
+        vector<int> dist(n + 1, INT_MAX);
+        // Step 4: Parent array to reconstruct the shortest path later
+        vector<int> parent(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;  // Initially, every node is its own parent
         }
-        dis[1]=0;
-        while(!pq.empty()){
-            auto it=pq.top();
-            int node=it.second;
-            int dist=it.first;
+        dist[1] = 0;  // Distance to the source node is 0
+        // Step 5: Dijkstra’s Algorithm main loop
+        while (!pq.empty()) {
+            int wt = pq.top().first;   // Current distance from source
+            int node = pq.top().second;
             pq.pop();
-            
-            for(auto it:adj[node]){
-                int adjnode=it.first;
-                int weight=it.second;
-                if(dist+weight<dis[adjnode]){
-                    dis[adjnode]=dist+weight;
-                    pq.push({dist+weight,adjnode});
-                    par[adjnode]=node;
+            // Traverse through all adjacent nodes
+            for (auto nbr : adjList[node]) {
+                int currWt = nbr.second;    // Weight of the edge to neighbour
+                int nbrNode = nbr.first;
+                // If a shorter path is found to neighbour
+                if (wt + currWt < dist[nbrNode]) {
+                    dist[nbrNode] = wt + currWt;   // Update distance
+                    pq.push({dist[nbrNode], nbrNode});  // Push updated distance into the min-heap
+                    parent[nbrNode] = node;  // Store the parent for path reconstruction
                 }
             }
         }
-        if(dis[n]==INT_MAX) return {-1};
-        vector<int >path;
-        int node=n;
-        while(par[node]!=node){
+        // If no path to node n, return -1
+        if (dist[n] == INT_MAX) return {-1};
+        // Step 6: Reconstruct the shortest path from node n to 1 using the parent array
+        vector<int> path;
+        int node = n;
+        // Traverse the parent chain until we reach the source
+        while (parent[node] != node) {
             path.push_back(node);
-            node=par[node];
+            node = parent[node];
         }
-        path.push_back(1);
-        path.push_back(dis[n]);
-        reverse(path.begin(),path.end());
+        path.push_back(1);         // Add the source node
+        path.push_back(dist[n]);   // Finally, push the shortest distance as the last element
+
+        // The path is currently from destination to source, so reverse it
+        reverse(path.begin(), path.end());
         return path;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    // your code goes here
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> edges;
-        map<pair<int, int>, int> mp;
-        for (int i = 0; i < m; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 3; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-            mp[{edges.back()[0], edges.back()[1]}] = edges.back()[2];
-            mp[{edges.back()[1], edges.back()[0]}] = edges.back()[2];
-        }
-
-        Solution obj;
-        vector<int> res = obj.shortestPath(n, m, edges);
-        int ans = 0;
-        for (int i = 2; i < res.size(); i++) {
-            ans += mp[{res[i], res[i - 1]}];
-        }
-        if (ans == res[0]) {
-
-        } else if (res.size() == 1 && res[0] == -1)
-            ans = res[0];
-        else
-            ans = -2;
-        cout << ans << endl;
-    }
-}
-
-// } Driver Code Ends
