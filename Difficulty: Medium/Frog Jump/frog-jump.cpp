81db514d8bf547f6cd8acc4f3f3dab54e4764c50
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2026-07-22 21:18:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,35 +1,19 @@
+// Memorization : 
 class Solution {
-  private:
-  // Brute DP Solution : Memorization : T.C: O(n), S.C: O(2*n) : Top-Down Approach
-  int findMinCost(int n, vector<int>& height, vector<int>& dp) {
-        if(n==0) return 0;
+    int minCostHelper(vector<int>& height, int n, vector<int>& dp) {
+        if(n == 0) return 0;
+        if(n == 1) return abs(height[1] - height[0]);
         if(dp[n] != -1) return dp[n];
-        int left = findMinCost(n-1, height, dp) + abs(height[n]-height[n-1]);
-        int right = INT_MAX;
-        if(n>1) right = findMinCost(n-2, height, dp) + abs(height[n]-height[n-2]);
-        return dp[n] = min(left, right);
+        int leftRecursion = minCostHelper(height, n-1, dp) + abs(height[n]-height[n-1]);
+        int rightRecursion = minCostHelper(height, n-2, dp) + abs(height[n]-height[n-2]);
+        return dp[n] = min(leftRecursion, rightRecursion);
     }
-    
-  // Pure recursive solution : T.C: O(2^n), S.C: O(n)
-    // int findMinCost(int n, vector<int>& height) {
-    //     if(n==0) return 0;
-    //     int left = findMinCost(n-1, height) + abs(height[n]-height[n-1]);
-    //     int right = INT_MAX;
-    //     if(n>1) right = findMinCost(n-2, height) + abs(height[n]-height[n-2]);
-    //     return min(left, right);
-    // }
-    
   public:
     int minCost(vector<int>& height) {
         // Code here
         int size = height.size();
-        // Pure Recursive Solution : 
-        // int ans = findMinCost(size-1, height);
-        
-        // Brute DP : Memorization 
-        vector<int> dp(size+1, -1);
-        int ans = findMinCost(size-1, height, dp);
-        
-        return ans;
+        vector<int> dp(size, -1);
+        return minCostHelper(height, size-1, dp);
     }
 };
+
